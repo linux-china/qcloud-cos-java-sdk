6abbef8edfcb88fd6f81dea24ec64eb792351e5a
linux_china <linux_china@hotmail.com>
2015-11-25 01:18:46 +0800

    COS Cloud

---
+++ src/main/java/com/qcloud/cos/api/CosCloud.java
@@ -0,0 +1,69 @@
+package com.qcloud.cos.api;
+
+import com.qcloud.cos.api.impl.BucketOperationImpl;
+
+/**
+ * COS Cloud
+ *
+ * @author linux_china
+ */
+public class CosCloud {
+    /**
+     * COS CGI URL
+     */
+    public static final String COSAPI_CGI_URL = "http://web.file.myqcloud.com/files/v1/";
+    /**
+     * APP ID
+     */
+    private int appId;
+    /**
+     * secretID
+     */
+    private String secretId;
+    /**
+     * secretKey
+     */
+    private String secretKey;
+    /**
+     * timeout
+     */
+    private int timeout;
+
+    /**
+     * CosCloud
+     *
+     * @param appId     APP ID
+     * @param secretId  secretID
+     * @param secretKey secretKey
+     */
+    public CosCloud(int appId, String secretId, String secretKey) {
+        this(appId, secretId, secretKey, 60);
+    }
+
+    /**
+     * CosCloud
+     *
+     * @param appId     APP ID
+     * @param secretId  secretID
+     * @param secretKey secretKey
+     * @param timeout   timeout
+     */
+    public CosCloud(int appId, String secretId, String secretKey, int timeout) {
+        this.appId = appId;
+        this.secretId = secretId;
+        this.secretKey = secretKey;
+        this.timeout = timeout * 1000;
+    }
+
+    /**
+     * get bucket operation
+     *
+     * @param bucketName bucket name
+     * @return bucket operation
+     */
+    public BucketOperation getBucketOperation(String bucketName) {
+        return new BucketOperationImpl(appId, secretId, secretKey, timeout, bucketName);
+    }
+
+
+}
